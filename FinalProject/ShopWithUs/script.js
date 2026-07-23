e3b3262b54d1aeb577902fff96d1a0532fd62ab8
Sneha Dahal <dahalsneha383@gmail.com>
2026-07-23 15:47:29 +0545

    Designed homepage

---
+++ FinalProject/ShopWithUs/script.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
